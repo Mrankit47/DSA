445a7f4c9c6a7a6737213b88cc018bcc37442494
Mrankit47 <ankitkushwah0210k@gmail.com>
2026-08-22 00:19:16 +0530

    bitwise operator

--- Lecture-7/lecture-7 questions/question2.cpp
+++ Lecture-7/lecture-7 questions/question2.cpp
@@ -1,1 +1,22 @@
 //write a function to reverse an Integer n.
+#include<iostream>
+using namespace std;
+
+int reverseint(int num)
+{
+    int rev;
+    for(int i=1; i<=num; i++)
+    {
+        
+    }
+}
+
+int main()
+{
+    int intnum;
+    cout<<"enter any integer number : ";
+    cin>>intnum;
+
+    reverseint(intnum);
+    return 0;
+}
