dd5bee6ec9d5083248111502192a2cf96aab7275
Mrankit47 <ankitkushwah0210k@gmail.com>
2026-09-24 08:26:30 +0530

    max subarray sum

---
+++ Lecture-10/Kadan's Algorithm/optimal solution/mss.cpp
@@ -0,0 +1,22 @@
+// Leetcode question no 53
+// Maximum subarray
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {5,7,-9,3,4,-2,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int sum = 0, maxsum = INT8_MIN;
+    for(int i=0; i<size; i++)
+    {
+        sum+=arr[i];
+        maxsum=max(sum,maxsum);
+        if(sum<0)
+        {
+            sum=0;
+        }
+    }
+    cout<<"max subarray sum : "<<sum;
+    return 0;
+}
